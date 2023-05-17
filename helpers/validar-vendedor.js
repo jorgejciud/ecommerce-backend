5cda882e867a807f5989d583bf1fc6567c31fc08
jorgejciud <jorge.julio@iudigital.edu.co>
2023-05-16 22:25:27 -0500

    Se arreglo la validacion del vendedor

--- helpers/validar-vendedor.js
+++ helpers/validar-vendedor.js
@@ -2,7 +2,7 @@ const validarVendedor = (req) => {
     const validaciones = [];
 
     if (!req.body.nombre) {
-        validaciones.push('El nombre de la categoria es requerido');
+        validaciones.push('El nombre del vendedor es requerido');
     }
 
     return validaciones;
